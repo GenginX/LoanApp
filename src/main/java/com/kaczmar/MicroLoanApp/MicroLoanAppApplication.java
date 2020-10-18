9f3a0cf067765a8a25b0e8e6e5a0a557a0e87ca0
Michal <0kaczmar9@gmail.com>
2020-10-18 16:07:52 +0200

    Add new feature Swagger

--- src/main/java/com/kaczmar/MicroLoanApp/MicroLoanAppApplication.java
+++ src/main/java/com/kaczmar/MicroLoanApp/MicroLoanAppApplication.java
@@ -2,9 +2,14 @@ package com.kaczmar.MicroLoanApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableSpringDataWebSupport
 public class MicroLoanAppApplication {
 
     public static void main(String[] args) {
